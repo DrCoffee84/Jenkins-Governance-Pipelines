d0ff4915af396c4039823debd128bee310a04656
unknown <daniel.boullon@hotmail.es>
2022-08-12 10:51:45 -0500

    fix folder name

---
+++ vars/runCIPipeline.groovy
@@ -0,0 +1,70 @@
+#!/usr/bin/env groovy
+
+def call(parameters) {
+    pipeline {
+        agent {
+            label parameters.agent
+        }
+        options {
+            skipDefaultCheckout()
+            disableConcurrentBuilds()
+        }
+        stages {
+            stage("Config WorkSpace"){
+                steps{
+                    print parameters
+                }
+            }
+            stage("Static Code Analysis") {
+                steps {
+                    print "todo implementacion"
+                }
+            }
+            stage("Build") {
+                steps {
+                    print "todo implementacion"
+                }
+            }
+            stage("Unit Test") {
+                steps {
+                    print "todo implementacion"
+                }
+            }
+            stage("Unit Integration Test") {
+                steps {
+                    print "todo implementacion"
+                }
+            }
+            stage("Code Coverage") {
+                steps {
+                    print "todo implementacion"                    
+                }
+            }
+            stage("Smart Code Coverage") {
+                steps {
+                    print "todo implementacion"                    
+                }
+            }
+            stage("Deployment To Dev") {
+                steps {
+                    print "todo implementacion"
+                }
+            }
+            stage("Post Deployment Test - Sanity Tests") {
+                steps {
+                    print "todo implementacion"
+                }
+            }
+            stage("Notification") {
+                steps {
+                   //env.WEBHOOK_CHANNELS = "${MICROSOFT_TEAMS_WEBHOOK_1},${MICROSOFT_TEAMS_WEBHOOK_2},${MICROSOFT_TEAMS_WEBHOOK_N}" 
+                }
+            }
+            stage("Dashboard") {
+                steps {
+                    print "todo implementacion"
+                }
+            }
+        }
+    }    
+}
