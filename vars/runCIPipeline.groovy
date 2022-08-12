7ef67762e8fc22d6d3c092c0bf76f710b5a3cf39
unknown <daniel.boullon@hotmail.es>
2022-08-12 11:09:07 -0500

    fix

--- vars/runCIPipeline.groovy
+++ vars/runCIPipeline.groovy
@@ -57,6 +57,7 @@ def call(parameters) {
             }
             stage("Notification") {
                 steps {
+                    print "todo implementacion"
                    //env.WEBHOOK_CHANNELS = "${MICROSOFT_TEAMS_WEBHOOK_1},${MICROSOFT_TEAMS_WEBHOOK_2},${MICROSOFT_TEAMS_WEBHOOK_N}" 
                 }
             }
